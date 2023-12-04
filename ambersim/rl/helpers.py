148c8d25afb1868b1d9ebe709fc9254e9956288b
Vince Kurtz <vjkurtz@gmail.com>
2023-12-04 14:23:11 -0800

    add flag for verifying network output sizes

--- ambersim/rl/helpers.py
+++ ambersim/rl/helpers.py
@@ -22,6 +22,7 @@ class BraxPPONetworksWrapper:
         observation_size: int,
         action_size: int,
         preprocess_observations_fn: types.PreprocessObservationFn = types.identity_observation_preprocessor,
+        check_sizes: bool = True,
     ) -> PPONetworks:
         """Create a PPONetworks object, compatible with brax's ppo.train() function.
 
@@ -29,6 +30,7 @@ class BraxPPONetworksWrapper:
             observation_size: Size of the input (observation).
             action_size: Size of the policy output (action).
             preprocess_observations_fn: Function to preprocess (e.g. normalize) observations.
+            check_sizes: Whether to check that the output sizes of the policy and value networks match the action and value distributions.
 
         Returns:
             A PPONetworks object.
@@ -37,17 +39,26 @@ class BraxPPONetworksWrapper:
         # parameters of this distribution.
         action_dist = self.action_distribution(event_size=action_size)
 
-        # Set up a dummy observation and random key for size verifications
+        # Set up a dummy observation for parameter initialization.
         dummy_observation = jnp.zeros((1, observation_size))
-        rng = jax.random.PRNGKey(0)
 
-        # Check that the output size of the policy network matches the size of
-        # the action distribution.
-        dummy_params = self.policy_network.init(rng, dummy_observation)
-        policy_output = self.policy_network.apply(dummy_params, dummy_observation)
-        assert (
-            policy_output.shape[-1] == action_dist.param_size
-        ), f"policy network output size {policy_output.shape[-1]} does not match action distribution size {action_dist.param_size}"
+        if check_sizes:
+            rng = jax.random.PRNGKey(0)
+
+            # Check that the output size of the policy network matches the size of
+            # the action distribution.
+            dummy_params = self.policy_network.init(rng, dummy_observation)
+            policy_output = self.policy_network.apply(dummy_params, dummy_observation)
+            assert (
+                policy_output.shape[-1] == action_dist.param_size
+            ), f"policy network output size {policy_output.shape[-1]} does not match action distribution size {action_dist.param_size}"
+
+            # Check that the output size of the value network is 1.
+            dummy_value_params = self.value_network.init(rng, dummy_observation)
+            value_output = self.value_network.apply(dummy_value_params, dummy_observation)
+            assert (
+                value_output.shape[-1] == 1
+            ), f"value network output size {value_output.shape} does not match expected size 1"
 
         # Create the policy network, a FeedForwardNetwork that contains an "init"
         # and an "apply" function.
@@ -61,12 +72,6 @@ class BraxPPONetworksWrapper:
             return self.policy_network.apply(policy_params, obs)
 
         # Create the value network. This is just like the policy network, but with a 1D output.
-        dummy_value_params = self.value_network.init(rng, dummy_observation)
-        value_output = self.value_network.apply(dummy_value_params, dummy_observation)
-        assert (
-            value_output.shape[-1] == 1
-        ), f"value network output size {value_output.shape} does not match expected size 1"
-
         def value_init(key):
             """Initialize the value network from a random key."""
             return self.value_network.init(key, dummy_observation)
